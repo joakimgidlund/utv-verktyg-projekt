7e9b619d767b7940b612a8f03d01330a299f4977
mariobugarin <mario00@live.se>
2024-12-11 14:19:18 +0100

    Added Product class, constructor and getters,setters

--- src/main/java/se/yrgo/inventory/libs/Product.java
+++ src/main/java/se/yrgo/inventory/libs/Product.java
@@ -1,4 +1,33 @@
 package se.yrgo.inventory.libs;
 
 public class Product {
+
+    private int price;
+    private String type;
+
+
+    public void Products(int price, String type) {
+        this.price = price;
+        this.type = type;
+
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+
 }
+
