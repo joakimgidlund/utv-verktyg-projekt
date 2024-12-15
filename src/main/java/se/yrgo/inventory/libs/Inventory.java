b96117f50fcc169523e2877226b7aa9d3688d109
jyoyu <jockegidlund@gmail.com>
2024-12-15 15:16:27 +0100

    Inventory updated to throw proper exceptions.

--- src/main/java/se/yrgo/inventory/libs/Inventory.java
+++ src/main/java/se/yrgo/inventory/libs/Inventory.java
@@ -4,24 +4,41 @@ import java.util.HashMap;
 
 public class Inventory {
 
-    private HashMap<Product, Integer> inventory;
+    private HashMap<Product, Integer> inventoryMap;
 
     public Inventory() {
-        inventory = new HashMap<Product, Integer>();
+        inventoryMap = new HashMap<Product, Integer>();
     }
 
-    public void addProduct(Product p, int quantity) {
-        inventory.put(p, inventory.get(p) + quantity);
+    public HashMap<Product, Integer> getInventory() {
+        return inventoryMap;
+    }
+
+    public void addProduct(Product p, int quantity) throws ProductAlreadyExistsException {
+        if(!inventoryMap.containsKey(p)) {
+            inventoryMap.put(p, quantity);
+        }
+        else {
+            throw new ProductAlreadyExistsException("Product already in inventory.");
+        }
     }
 
     public void removeProduct(Product p) {
-        inventory.remove(p);
+        inventoryMap.remove(p);
     }
 
     public int getProductInventory(Product p) {
-        return inventory.get(p);
+        return inventoryMap.get(p);
     }
 
+    public void addProductInventory(Product p, int quantity) throws ProductNotFoundException {
+        if(inventoryMap.containsKey(p)) {
+            inventoryMap.put(p, inventoryMap.get(p) + quantity);
+        }
+        else {
+            throw new ProductNotFoundException("No such product in inventory.");
+        }
+    }
     @Override
     public String toString() {
         return String.format("");
