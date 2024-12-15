0212616b16661e04e1161ff7022b99414d0644e6
jyoyu <jockegidlund@gmail.com>
2024-12-15 15:10:31 +0100

    Created exceptions for inventory management.

---
+++ src/main/java/se/yrgo/inventory/libs/ProductAlreadyExistsException.java
@@ -0,0 +1,8 @@
+package se.yrgo.inventory.libs;
+
+public class ProductAlreadyExistsException extends Exception {
+    private static final long serialVersionUID = 9123L;
+
+    public ProductAlreadyExistsException(String s) {
+    }
+}
---
+++ src/main/java/se/yrgo/inventory/libs/ProductNotFoundException.java
@@ -0,0 +1,8 @@
+package se.yrgo.inventory.libs;
+
+public class ProductNotFoundException extends Exception {
+    private static final long serialVersionUID = 12311L;
+
+    public ProductNotFoundException(String s) {
+    }
+}
