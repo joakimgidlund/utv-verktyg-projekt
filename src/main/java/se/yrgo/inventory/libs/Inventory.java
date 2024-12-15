943d5ffe9433c595a0e716c28fd8d92a24bf4788
jyoyu <jockegidlund@gmail.com>
2024-12-15 14:20:19 +0100

    Inventory skeleton created.

--- src/main/java/se/yrgo/inventory/libs/Inventory.java
+++ src/main/java/se/yrgo/inventory/libs/Inventory.java
@@ -1,4 +1,29 @@
 package se.yrgo.inventory.libs;
 
+import java.util.HashMap;
+
 public class Inventory {
+
+    private HashMap<Product, Integer> inventory;
+
+    public Inventory() {
+        inventory = new HashMap<Product, Integer>();
+    }
+
+    public void addProduct(Product p, int quantity) {
+        inventory.put(p, inventory.get(p) + quantity);
+    }
+
+    public void removeProduct(Product p) {
+        inventory.remove(p);
+    }
+
+    public int getProductInventory(Product p) {
+        return inventory.get(p);
+    }
+
+    @Override
+    public String toString() {
+        return String.format("");
+    }
 }
