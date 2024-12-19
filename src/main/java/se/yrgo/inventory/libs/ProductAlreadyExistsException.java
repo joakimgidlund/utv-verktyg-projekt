5a682f17031255effae85d8eecc90f6057e1ad57
jyoyu <jockegidlund@gmail.com>
2024-12-19 08:22:42 +0100

    Removed ProductAlreadyExistsException

--- src/main/java/se/yrgo/inventory/libs/ProductAlreadyExistsException.java
+++
@@ -1,9 +0,0 @@
-package se.yrgo.inventory.libs;
-
-public class ProductAlreadyExistsException extends Exception {
-    private static final long serialVersionUID = 9123L;
-
-    public ProductAlreadyExistsException(String s) {
-        super(s);
-    }
-}
